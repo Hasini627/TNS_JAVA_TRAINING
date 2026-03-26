2fb1d7cbb0c04101cf51c4cea2d754186bbfe4cd
Hasini627 <1dt23cs074@dsatm.edu.in>
2026-03-26 22:27:22 +0530

    first commit

---
+++ DSATM/src/org/tnsif/cap/c2tc/finaldemo/FinalClassDemo.java
@@ -0,0 +1,21 @@
+package org.tnsif.cap.c2tc.finaldemo;
+
+final class ImplementByHema{
+	public void show()
+	{
+		System.out.println("Ai integration done by hema");
+	}
+}
+class Vishu extends ImplementByHema{
+	public void show()
+	{
+		System.out.println("Ai integration done by vishu");
+	}
+}
+public class FinalClassDemo {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
