186f473931df545c6ca7bdd9c3f712b19e4c6832
Hasini627 <1dt23cs074@dsatm.edu.in>
2026-04-13 13:19:56 +0530

    first commit

---
+++ DSATM/src/org/tnsif/cap/c2tc/java/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.tnsif.cap.c2tc.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello students ");
+	
+
+	}
+
+}
---
+++ DSATM/src/org/tnsif/cap/c2tc/java/IfDemo.java
@@ -0,0 +1,17 @@
+package org.tnsif.cap.c2tc.java;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int age=11;
+		if (age>18)
+		{
+			System.out.println("you are eligible");
+		}
+		else
+		{
+			System.out.println("you are not eligible");
+		}
+	}
+
+}
